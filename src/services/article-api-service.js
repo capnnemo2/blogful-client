177e65706cd97262dcab9454acf58ca698782454
Ben Hernandez <benjammin234@gmail.com>
2020-05-08 10:11:01 -0700

    All hooked up for jwt

--- src/services/article-api-service.js
+++ src/services/article-api-service.js
@@ -4,27 +4,27 @@ import config from "../config";
 const ArticleApiService = {
   getArticles() {
     return fetch(`${config.API_ENDPOINT}/articles`, {
-      headers: {}
-    }).then(res =>
-      !res.ok ? res.json().then(e => Promise.reject(e)) : res.json()
+      headers: {},
+    }).then((res) =>
+      !res.ok ? res.json().then((e) => Promise.reject(e)) : res.json()
     );
   },
   getArticle(articleId) {
     return fetch(`${config.API_ENDPOINT}/articles/${articleId}`, {
       headers: {
-        authorization: `basic ${TokenService.getAuthToken()}`
-      }
-    }).then(res =>
-      !res.ok ? res.json().then(e => Promise.reject(e)) : res.json()
+        authorization: `bearer ${TokenService.getAuthToken()}`,
+      },
+    }).then((res) =>
+      !res.ok ? res.json().then((e) => Promise.reject(e)) : res.json()
     );
   },
   getArticleComments(articleId) {
     return fetch(`${config.API_ENDPOINT}/articles/${articleId}/comments`, {
       headers: {
-        authorization: `basic ${TokenService.getAuthToken()}`
-      }
-    }).then(res =>
-      !res.ok ? res.json().then(e => Promise.reject(e)) : res.json()
+        authorization: `bearer ${TokenService.getAuthToken()}`,
+      },
+    }).then((res) =>
+      !res.ok ? res.json().then((e) => Promise.reject(e)) : res.json()
     );
   },
   postComment(articleId, text) {
@@ -32,16 +32,16 @@ const ArticleApiService = {
       method: "POST",
       headers: {
         "content-type": "application/json",
-        authorization: `basic ${TokenService.getAuthToken()}`
+        authorization: `bearer ${TokenService.getAuthToken()}`,
       },
       body: JSON.stringify({
         article_id: articleId,
-        text
-      })
-    }).then(res =>
-      !res.ok ? res.json().then(e => Promise.reject(e)) : res.json()
+        text,
+      }),
+    }).then((res) =>
+      !res.ok ? res.json().then((e) => Promise.reject(e)) : res.json()
     );
-  }
+  },
 };
 
 export default ArticleApiService;
